fd7e1849a5fa7669ac9a9fa1a49b98815893bccc
Snapview-team <lcgeming24@gmail.com>
2025-06-08 22:59:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<script>
 function submitScore() {
   const score = parseInt(document.getElementById("viewScore").value);
   if (score < 3) {
@@ -8,13 +7,13 @@ function submitScore() {
   }
 }
 
+// 地图初始化
 const map = L.map('map').setView([47.5608, -122.0651], 14);
+
 L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
   attribution: 'Map data © OpenStreetMap contributors, HOT'
 }).addTo(map);
 
-}).addTo(map);
 L.marker([47.5608, -122.0651]).addTo(map)
   .bindPopup('Lake Sammamish State Park')
   .openPopup();
-<script>
